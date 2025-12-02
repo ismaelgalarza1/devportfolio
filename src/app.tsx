726c9dbda954366bb20a23254935c49705a99130
Ismael G <underthehaze13@outlook.com>
2025-12-01 19:37:54 -0500

    added other changes to my social media icons, and the 404 issues when refreshing.

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/mainPage";
 import ContactPage from "./pages/contact";
 import Footer from "./components/footer";
